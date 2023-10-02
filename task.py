0844087901fc5de6bbc36a192a88a0ddd8c6037e
Samuel Vandenbussche <91118298+SamuelVandenbussche@users.noreply.github.com>
2023-10-02 13:41:44 +0200

    Update task.py

--- task.py
+++ task.py
@@ -4,57 +4,59 @@ from simpleai.search import CspProblem, backtrack
 # Function to solve the cryptarithmetic puzzle
 def solve_puzzle(puzzle):
     words = puzzle.split(" ")
-
-    variables = []
-    domains = {}
-
-    for word in words:
-        for char in word:
-            if char != "=" and char != "+":
-                variables += char
-    variables = set(variables)
-
-    for word in words:
-        for i, char in enumerate(word):
-            if char != "=" and char != "+":
-                if i == 0:
-                    domains[char] = list(range(1, 10))
-                else:
-                    domains[char] = list(range(10))
-
-    def constraint_unique(variables, values):
-        return len(values) == len(set(values))
-
-    def constraint_add(variables, values):
-        factor1 = ""
-        factor2 = ""
-        result = ""
-        for char in words[0]:
-            position = variables.index(char)
-            factor1 += str(values[position])
-        factor1 = int(factor1)
-        for char in words[2]:
-            position = variables.index(char)
-            factor2 += str(values[position])
-        factor2 = int(factor2)
-        for char in words[4]:
-            position = variables.index(char)
-            result += str(values[position])
-        result = int(result)
-        return (factor1 + factor2) == result
-
-    constraints = [
-        (variables, constraint_unique),
-        (variables, constraint_add),
-    ]
-    problem = CspProblem(variables, domains, constraints)
-
-    output = backtrack(problem)
-    if output:
-        solution = {var: output[var] for var in variables}
-        return solution
+    if len(words) != 5 or words[1] != '+' or words[3] != '=':
+        st.write("Invalid input. Please enter a cryptarithmetic puzzle with 2 factors.")
     else:
-        return None
+        variables = []
+        domains = {}
+    
+        for word in words:
+            for char in word:
+                if char != "=" and char != "+":
+                    variables += char
+        variables = set(variables)
+    
+        for word in words:
+            for i, char in enumerate(word):
+                if char != "=" and char != "+":
+                    if i == 0:
+                        domains[char] = list(range(1, 10))
+                    else:
+                        domains[char] = list(range(10))
+    
+        def constraint_unique(variables, values):
+            return len(values) == len(set(values))
+    
+        def constraint_add(variables, values):
+            factor1 = ""
+            factor2 = ""
+            result = ""
+            for char in words[0]:
+                position = variables.index(char)
+                factor1 += str(values[position])
+            factor1 = int(factor1)
+            for char in words[2]:
+                position = variables.index(char)
+                factor2 += str(values[position])
+            factor2 = int(factor2)
+            for char in words[4]:
+                position = variables.index(char)
+                result += str(values[position])
+            result = int(result)
+            return (factor1 + factor2) == result
+    
+        constraints = [
+            (variables, constraint_unique),
+            (variables, constraint_add),
+        ]
+        problem = CspProblem(variables, domains, constraints)
+    
+        output = backtrack(problem)
+        if output:
+            solution = {var: output[var] for var in variables}
+            return solution
+        else:
+            return None
 
 # Streamlit app title and description
 st.title("Cryptarithmetic Puzzle Solver")
